91429ca4d80ef7b91a8b404883c0577eab89f64b
Antonio Fernández Vara <a.fernandez@retocontinuo.com>
2011-09-19 16:11:12 +0200

    rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tabelia::Application.config.secret_token = '1f2f9f5e120dc99b198783933f188a6db4222369b8124a6823fcaeeb5a207942a09c7cf7585480f71e49c9595f3e62dc9c4b81897ec9591f7cf20bb188bd6c31'
