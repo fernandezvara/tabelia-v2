e8547408dd39ea68d406e277f33eb968807a810b
Antonio Fernández Vara <a.fernandez@retocontinuo.com>
2011-09-28 17:49:18 +0200

    cleaning...

--- test/unit/follower_backward_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FollowerBackwardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/follower_forward_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FollowerForwardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
